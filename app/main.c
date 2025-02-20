f7664ced9ca5149246a11951d2ff7647330d6a67
ayo_surya <evedansh123@gmail.com>
2025-02-20 13:29:51 +0530

    Added a while to take more inputs.

--- app/main.c
+++ app/main.c
@@ -6,13 +6,17 @@ int main() {
   setbuf(stdout, NULL);
 
   // Uncomment this block to pass the first stage
-  printf("$ ");
-
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  input[strlen(input)-1] = '\0';
   
-  printf("%s: command not found\n", input);
+  char input[100];
+  while (1)
+  {
+    printf("$ ");
+
+    fgets(input, 100, stdin);
+    input[strlen(input)-1] = '\0';
+    
+    printf("%s: command not found\n", input);
+  }
+  
   return 0;
 }
