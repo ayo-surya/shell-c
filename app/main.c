f55b0425f9b2b0c0d8cf215c85231c304adf4d4d
ayo_surya <evedansh123@gmail.com>
2025-02-20 13:47:19 +0530

    Added the exit call

--- app/main.c
+++ app/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main() {
   // Flush after every printf
@@ -14,6 +15,10 @@ int main() {
 
     fgets(input, 100, stdin);
     input[strlen(input)-1] = '\0';
+
+    if(!strcmp(input, "exit 0")){
+        exit(0);
+    }
     
     printf("%s: command not found\n", input);
   }
